49fd76db4a3e2e635683871b045999a09145a2a9
seanbetts <seanbetts@icloud.com>
2022-05-27 17:51:43 +0100

    Customised Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
+import discord from '../../assets/discord.png';
+import twitter from '../../assets/twitter.png';
+import instagram from '../../assets/instagram.png';
 import './navbar.css';
 
 const Menu = () => (
   <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#wgpt3">What is GPT3?</a></p>
-  <p><a href="#possibility">Open AI</a></p>
-  <p><a href="#features">Case Studies</a></p>
-  <p><a href="#blog">Library</a></p>
+  <p><a href="#home">Projects</a></p>
+  <p><a href="#wgpt3">Products</a></p>
+  <p><a href="#possibility">About Us</a></p>
+  <p><a href="#features">Support</a></p>
   </>
 )
 
@@ -27,8 +29,18 @@ const Navbar = () => {
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
-        <p>Sign In</p>
-        <button type="button">Sign Up</button>
+        <div className='gpt3__navbar-sign-socials'>
+          <a href="https://www.discord.com" target="_blank" rel="noopener noreferrer">
+            <img src={discord} alt="discord" />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={instagram} alt="instagram" />
+          </a>
+        </div>
+        <button type="button">CONNECT</button>
       </div>
       <div className='gpt3__navbar-menu'>
         {toggleMenu
@@ -40,8 +52,18 @@ const Navbar = () => {
             <div className='spt3__navbar-menu_container-links'>
               <Menu />
                 <div className='gpt3__navbar-menu_container-links-sign'>
-                  <p>Sign In</p>
-                  <button type="button">Sign Up</button>
+                <div className='gpt3__navbar-sign-socials'>
+                  <a href="https://www.discord.com" target="_blank" rel="noopener noreferrer">
+                    <img src={discord} alt="discord" />
+                  </a>
+                  <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                    <img src={twitter} alt="twitter" />
+                  </a>
+                  <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <img src={instagram} alt="instagram" />
+                  </a>
+                </div>
+                  <button type="button">CONNECT</button>
                 </div>
             </div>
           </div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,9 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      Header
+    </div>
   )
 }
 
