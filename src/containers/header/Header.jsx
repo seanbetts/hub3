1a07819af4750fdd5782b9428facb1d6b56975e6
seanbetts <seanbetts@icloud.com>
2022-05-27 19:49:11 +0100

    Added project images

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './header.css';
 import visitor from '../../assets/visitor.png';
+import darcel from '../../assets/darcel.png';
+import burb from '../../assets/burb.png';
+import lizard from '../../assets/lizard.png';
+import shroom from '../../assets/shroom.png';
+import deadfella from '../../assets/deadfella.png';
 
 const Header = () => {
   return (
@@ -12,11 +17,11 @@ const Header = () => {
       </div>
       <div className='gpt3__header-image'>
         <img src={visitor} alt="visitor" />
-        <img src={visitor} alt="visitor" />
-        <img src={visitor} alt="visitor" />
-        <img src={visitor} alt="visitor" />
-        <img src={visitor} alt="visitor" />
-        <img src={visitor} alt="visitor" />
+        <img src={darcel} alt="darcel" />
+        <img src={burb} alt="burb" />
+        <img src={lizard} alt="lizard" />
+        <img src={shroom} alt="shroom" />
+        <img src={deadfella} alt="deadfella" />
       </div>
     </div>
   )
